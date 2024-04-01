2bab1fce37f8f14c11cc26bea95a81104785a4d8
root <root@evzawdapvl.local>
2024-04-01 20:18:26 +0000

    smth 1

--- min_social/min_social/settings.py
+++ min_social/min_social/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'min_social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postrgresql_psycopg2',
+        'NAME': 'test1',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
